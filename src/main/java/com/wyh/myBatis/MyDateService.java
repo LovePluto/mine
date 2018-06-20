c8bbbc886b8ab4ab6ca92aa22939a07eda78c21c
LovePluto <591712730@qq.com>
2018-06-20 11:33:13 +0800

    06201133

--- src/main/java/com/wyh/myBatis/MyDateService.java
+++ src/main/java/com/wyh/myBatis/MyDateService.java
@@ -6,10 +6,11 @@ import java.util.Date;
 import java.util.List;
 
 public class MyDateService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        getAll();
 //        selectAll();
         insertDate();
+//        System.out.println(new SimpleDateFormat("yyyy-MM-dd").parse(System.currentTimeMillis() + ""));
     }
 
     public static void getAll() {
@@ -26,16 +27,16 @@ public class MyDateService {
         for (MyDate myDate : list) {
             System.out.println(myDate.toString());
         }
+
     }
 
-    public static void insertDate() {
+    public static void insertDate() throws Exception {
         SqlSession session = DBTools.getSession();
         MyDateMapper mapper = session.getMapper(MyDateMapper.class);
         MyDate myDate = new MyDate();
         myDate.setName("555");
-//        myDate.setId(5);
-        Date date =new Date();
-        myDate.setStartTime(new Date());
+        Date date = new Date();
+        myDate.setStartTime(date);
         System.out.println(mapper.insertDate(myDate));
         session.commit();
 
