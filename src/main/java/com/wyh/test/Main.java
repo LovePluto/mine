834b51d1f3f41decb4d03a21147dfde111e94baa
wangyuheng <yuheng.wang@downjoy.com>
2018-07-19 18:06:43 +0800

    1606

--- src/main/java/com/wyh/test/Main.java
+++ src/main/java/com/wyh/test/Main.java
@@ -2,25 +2,24 @@ package com.wyh.test;
 
 //import com.sun.org.apache.xpath.internal.operations.String;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String a = "hello";
-        String b = "hello2";
-        String c = a + 2;
-        if (c == b) {
-            System.out.println("true");
-        }
 
-        byte[] bs = new byte[2];
-        bs[0] = -127;
-        bs[1] = -127;
-        String s = new String(bs, "utf-16");
-        System.out.println(s);
-       byte[] aa =  "\udbdb".getBytes("utf-16");
-       for (byte bvv:aa){
-           System.out.println(bvv);
-       }
+        int a = 1;
+        a = a++;
+        System.out.println(a);
 
+        LinkedHashMap map = new LinkedHashMap();
+
+//        AtomicInteger
+//        System.out.println(b);
+//        System.out.println(Math.pow(2,7));
+//        System.out.println(Math.pow(2,15));
+//        System.out.println(Math.pow(2,31));
     }
 }
